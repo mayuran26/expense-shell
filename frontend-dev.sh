06f8f91a0f4438bbebc4d96c911c4a32b4cd08d3
mayuran26 <bcamayu26@gmail.com>
2023-11-10 15:51:36 +0100

    added frontend-dev.sh and expense.conf files

---
+++ frontend-dev.sh
@@ -0,0 +1,17 @@
+dnf install nginx -y
+
+rm -rf /usr/share/nginx/html/*
+
+cp expense.conf /etc/nginx/default.d/expense.conf
+
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+
+systemctl enable nginx
+
+systemctl restart nginx
+
+
+
